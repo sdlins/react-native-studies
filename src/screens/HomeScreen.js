ece23f167b7de3ae15de942d085883bcaeb9a6c6
Sidney Lins <sdlins.dev@gmail.com>
2018-12-08 21:54:57 -0200

    Toggle text color for even odd counter

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,33 @@
 import React, { Component } from 'react'
-import { View, Text, ImageBackground, Image, TouchableWithoutFeedback } from 'react-native'
+import { StyleSheet, View, Text, ImageBackground, Image, TouchableWithoutFeedback } from 'react-native'
+
+const estilos = StyleSheet.create({
+    fundo: {
+        flex: 1,
+        alignItems: 'stretch',
+        justifyContent: 'center',
+    },
+    wrapperLogo: {
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        paddingBottom: 32
+    },
+    wrapperLogoCaixa: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    fundoBotao: {
+        backgroundColor: 'white',
+        paddingBottom: 16,
+        paddingTop: 16
+    },
+    textoBotao: {
+        textAlign: 'center',
+        fontSize: 18,
+    }
+})
 
 class HomeScreen extends Component {
     state = {
@@ -13,41 +41,22 @@ class HomeScreen extends Component {
     }
 
     render() {
+        const estiloDoTexto = this.state.contador % 2 === 0 ? { color: 'red' } : null
         return (
             <ImageBackground
                 source={require('../../assets/fundo.png')}
                 imageStyle={{ resizeMode: 'stretch' }}
-                style={{
-                    flex: 1,
-                    alignItems: 'stretch',
-                    justifyContent: 'center',
-                }}
+                style={estilos.fundo}
             >
-                <View style={{
-                    flex: 1,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}>
+                <View style={estilos.wrapperLogoCaixa}>
                     <Image source={require('../../assets/logo-caixa.png')}/>
                 </View>
-                <View style={{
-                    flexDirection: 'row',
-                    alignItems: 'flex-end',
-                    justifyContent: 'center',
-                    paddingBottom: 32
-                }}>
+                <View style={estilos.wrapperLogo}>
                     <Image source={require('../../assets/logo.png')}/>
                 </View>
                 <TouchableWithoutFeedback onPress={this.tratarContador}>
-                    <View style={{
-                        backgroundColor: 'white',
-                        paddingBottom: 16,
-                        paddingTop: 16
-                    }}>
-                        <Text style={{
-                            textAlign: 'center',
-                            fontSize: 18,
-                        }}>Começar! {this.state.contador} </Text>
+                    <View style={estilos.fundoBotao}>
+                        <Text style={[estilos.textoBotao, estiloDoTexto]}>Começar! {this.state.contador} </Text>
                     </View>
                 </TouchableWithoutFeedback>
             </ImageBackground>
