92285a863e0e0a97968f986f81fcccd9d1229db6
Sidney Lins <sdlins.dev@gmail.com>
2018-12-09 02:09:11 -0200

    Adds trip flatlist

--- src/screens/ViagensScreen/index.js
+++ src/screens/ViagensScreen/index.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
-import { View, Text, Dimensions } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import Viagem from './Viagem'
 
 export default class ViagensScreen extends Component {
     static navigationOptions = {
         header: null
     }
+
     render() {
+        const viagens = [
+            { id: 1, destino: 'Casa ABC', preco: 'R$ 350' },
+            { id: 2, destino: 'Morro XYZ', preco: 'R$ 889' }
+        ]
+
+        renderizarViagem = ({item}) => {
+            return <Viagem destino={item.destino} preco={item.preco} />
+        }
+
         return (
             <View style={{
                 flex: 1,
@@ -18,7 +28,14 @@ export default class ViagensScreen extends Component {
                 }}>
                     <Text>Mapa</Text>
                 </View>
-                <Viagem destino='Viagem ABC' preco='R$ 3000' />
+                <View>
+                    <FlatList
+                        data={viagens}
+                        renderItem={renderizarViagem}
+                        keyExtractor={item => item.id.toString()}
+                        horizontal
+                    />
+                </View>
             </View>
         )
     }
